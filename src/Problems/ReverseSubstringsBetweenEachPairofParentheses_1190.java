d26e119e34055993646ee5de4d257011c716e26b
Mohanraj <mohanrajacc0@gamil.com>
2024-07-11 21:38:26 +0530

    11/7/24

--- src/Problems/ReverseSubstringsBetweenEachPairofParentheses_1190.java
+++ src/Problems/ReverseSubstringsBetweenEachPairofParentheses_1190.java
@@ -1,7 +1,48 @@
 package Problems;
 
+import java.util.Stack;
+
 public class ReverseSubstringsBetweenEachPairofParentheses_1190 {
     public static void main(String[] args) {
+        String s = "(u(love)i)";
+
+        StringBuilder rs = new StringBuilder(s);
+
+        int startind = rs.lastIndexOf("(");
+        int endind = rs.indexOf(")",startind);
+
+        while (startind != -1){
+            StringBuilder ss = new StringBuilder(rs.substring(startind+1,endind));
+            ss.reverse();
+            rs.replace(startind,endind+1,ss.toString());
+            startind = rs.lastIndexOf("(");
+            endind = rs.indexOf(")", startind);
+        }
+
+        System.out.println(rs);
+
+
+
+//        StringBuilder ans = new StringBuilder();
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.charAt(i) == '('){
+//
+//            }
+//        }
+//
+//        for (int i = s.length()-1; i >=0; i--) {
+//            if (s.charAt(i) == ' ' || s.charAt(i) == ')' || s.charAt(i) == '(' ){
+//                continue;
+//            }else {
+//                ans.append(s.charAt(i));
+//            }
+//        }
+//
+//        System.out.println(ans);
+
+
+
 
     }
 }
